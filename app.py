3ab26ea60a23ad4268379b6a348e145d7bf60774
Ubuntu <ubuntu@ip-172-31-21-158.ap-northeast-1.compute.internal>
2017-10-18 04:14:52 +0000

    Added redis and rethinkdb

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import json
 
 from bs4 import BeautifulSoup
 import requests
-from flask import Flask, request
+from flask import Flask, request, g, abort
+import redis
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
+
 app = Flask(__name__)
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
@@ -12,6 +16,37 @@ app.logger.setLevel(logging.DEBUG)
 
 VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 PAGE_ACCESS_TOKEN = os.getenv('PAGE_ACCESS_TOKEN')
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_NO = os.getenv('REDIS_NO')
+
+RDB_HOST =  os.environ.get('RDB_HOST') or 'localhost'
+RDB_PORT = os.environ.get('RDB_PORT') or 28015
+RDB_DB = 'bus_bot'
+
+ERROR_MSG = '您輸入的公車名稱有誤'
+
+def init_redis():
+    db = redis.StrictRedis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_NO)
+    return db
+
+@app.before_request
+def before_request():
+    try:
+        g.rdb_conn = r.connect(host=RDB_HOST, port=RDB_PORT, db=RDB_DB)
+        g.redis = init_redis()
+    except RqlDriverError:
+        abort(503, "No database connection could be established.")
+
+@app.teardown_request
+def teardown_request(exception):
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
 
 def get_time_by_route(route_name):
     res = requests.post("http://pda.5284.com.tw/MQS/businfo2.jsp?routename=%s"%route_name)
@@ -22,14 +57,17 @@ def get_time_by_route(route_name):
     dirs = {'go':  [tr for tr in [tr for tr in soup.select('tr') if tr.get('class')] if 'ttego' in ''.join(tr['class'])],
             'back': [tr for tr in [tr for tr in soup.select('tr') if tr.get('class')] if 'back' in ''.join(tr['class'])]}
     data = {}
+    _id = 0
     for k, _dir in dirs.items():
         d = []
         for tr in _dir:
             res = {}
             td = tr.select('td')
+            res['id'] = _id
             res['stn'] = td[0].text
             res['status'] = td[1].text
             d.append(res)
+            _id += 1
         data[k] = d
     return data
 
@@ -43,16 +81,47 @@ def send_msg(data):
         app.logger.info(res.text)
 
 def send_text_msg(recipient_id, message_text):
+    chunk_size = 640
+    r = ''
+    dd = []
+    for row in message_text.split('\n'):
+        if len(r+row+'\n') > chunk_size:
+            dd.append(r)
+            r = ''
+        r = r+row+'\n'
+    dd.append(r)
+    for chunk in dd:
+        data = {
+            "recipient": {"id": recipient_id},
+            "message": {"text": chunk}
+        }
+        send_msg(data)
+def send_quick_reply(recipient_id, message_text):
     data = {
         "recipient": {"id": recipient_id},
-        "message": {"text": message_text}
+        "message": {"text": message_text,"quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Search",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/red.png"
+      },
+      {
+        "content_type":"location"
+      },
+      {
+        "content_type":"text",
+        "title":"Something Else",
+        "payload":"<POSTBACK_PAYLOAD>"
+      }
+    ]}
     }
     send_msg(data)
 
 def render_res(data):
     res = ""
     for row in data:
-        res = res + "%s -> %s\n"%(row['stn'],row['status'])
+        res = res + "%s: %s -> %s\n"%(row['id'],row['stn'],row['status'])
     return res
         
 
@@ -81,8 +150,9 @@ def webhook():
                             for k in data:
                                 res = render_res(data[k])
                                 send_text_msg(event.get('sender').get('id'), res)
+                            send_quick_reply(event.get('sender').get('id'), "請選擇想要推播的站牌id")
                         else:
-                            send_text_msg(event.get('sender').get('id'), 'bad bus')
+                            send_text_msg(event.get('sender').get('id'), ERROR_MSG)
                           
                     else:
                         app.logger.debug('Webhook received unknow event: %s'%event)
